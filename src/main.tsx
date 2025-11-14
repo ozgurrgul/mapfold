36983046b8a5bb41b4262115ec61abece127a089
ozgur <lugrugzo@gmail.com>
2025-11-14 10:04:37 +0300

    chore: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "./index.css";
 import store from "./store/store.ts";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </StrictMode>
 );
